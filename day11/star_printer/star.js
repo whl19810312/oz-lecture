f7d773ddf39234b3453ce1e273aa062013eca3a2
Wonho Lee <whl19810312@gmail.com>
2025-07-10 17:25:24 +0900

    주석 제거

--- day11/star_printer/star.js
+++ day11/star_printer/star.js
@@ -63,20 +63,17 @@ function print_star_box(count) {
 
 function main() {	
 	console.log('start');
-	const MAX = 100; // const 사용 예시
-	let input = prompt("별 갯수를 입력하세요.");          // 문자열 입력
-	let count = Number(input);                          // 숫자로 변환
+	const MAX = 100; 
+	let input = prompt("별 갯수를 입력하세요.");
+	let count = Number(input);      
 	
-	// 입력 유효성 검사 (도전 과제)
 	if (validation(count, MAX) == false) {
 		console.log(`Invalid count! Please enter a number between 0 and ${MAX}.`);
 		return;
 	}
 
-	// 점수 반올림
 	count = Math.round(count);
 
-	// 최종 점수 제한 (100 넘을 경우)
 	if (count > MAX) {
 		count = MAX;
 	}
