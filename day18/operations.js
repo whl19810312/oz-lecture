09411fea03cf3059a7b632da7eb87c87e134fe75
Wonho Lee <whl19810312@gmail.com>
2025-07-21 15:21:17 +0900

    day18/operations

---
+++ day18/operations.js
@@ -0,0 +1,12 @@
+export default function calculateOperation(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+': return firstNumber + secondNumber;
+        case '-': return firstNumber - secondNumber;
+        case '*': return firstNumber * secondNumber;
+        case '/':
+            if (secondNumber === 0) throw new Error("0으로 나눌 수 없습니다.");
+            return firstNumber / secondNumber;
+        default:
+            throw new Error("유효하지 않은 연산자입니다.");
+    }
+}
